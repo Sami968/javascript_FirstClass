6f75c4d3f17b8efec87658b9347e17872f538709
Sami968 <145800828+Sami968@users.noreply.github.com>
2023-09-23 08:29:10 +0000

    2nd class done

---
+++ 01_basics/02_variavles.js
@@ -0,0 +1,7 @@
+const accountId=12345;
+let accountName= "Sami";
+var accountFrstName= "Rana";
+accountName1= "Rana Sami";
+console.table([accountId, accountName, accountFrstName, accountName1]);
+// console.log("accountId"); we can not use such type of data accountId=1387;
+
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,6 @@
 console.log("Sami")
+console.log("Asad")
+console.log("Memoona")
+console.log("Rabia")
+console.log("Nasira Parveen")
+console.log("Saleem")
