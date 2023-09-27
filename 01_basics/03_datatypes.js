b42de3eba3677a84fc4a4405d5909453ce38addb
Sami968 <145800828+Sami968@users.noreply.github.com>
2023-09-27 09:01:50 +0000

    done

--- 01_basics/03_datatypes.js
+++ 01_basics/03_datatypes.js
@@ -1,8 +1,18 @@
 // here we study different datatypes in javascript some examples are as follow as!
-
+ let number= 34
 // number= 34  number is a data type in javascript
 // string = " " in double quotation we can written any data
 // boolean = (true or false) is also data type value must be true or false 
 // null (represent null value)
 // undefined (represent undefined value)
 // isLoggedIn
+// symbol => unique 
+let isLoggedIn=false;
+
+// objects 
+
+console.log(typeof null);
+console.log(typeof " ");
+console.log(typeof undefined);
+console.log(typeof number);
+console.log(typeof isLoggedIn);
---
+++ 01_basics/04_conversionoperation.js
@@ -0,0 +1,10 @@
+ 
+ let tem= undefined
+
+ console.log(typeof tem);
+
+ let valueInNumber= Number(tem)
+
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
