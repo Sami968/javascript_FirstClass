16c4b4bbf11f7c2847bf4873423ca13f0b3315f6
Sami968 <145800828+Sami968@users.noreply.github.com>
2023-10-08 19:17:05 +0000

    math remaning

---
+++ 01_basics/07_num_math.js
@@ -0,0 +1,30 @@
+const marks=100       // this method specified data type can be number or maybe different
+
+console.log(marks);
+console.log(typeof marks);
+
+const newMarks=new Number(99)    // this method specified data type called number
+console.log(newMarks);
+console.log(typeof newMarks);
+
+
+const testNum= 1005678.8976
+console.log(testNum);
+
+console.log(testNum.toString().length);
+ // console.log(typeof testNum);
+
+ //console.log(testNum.toFixed(2));
+
+//onsole.log(testNum.toPrecision(3));
+
+console.log(testNum.toLocaleString());
+
+// ***************************** Maths   *****************************************************
+
+
+
+
+
+
+
